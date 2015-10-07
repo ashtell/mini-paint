be7188d1dd0e03e1a85900d979ac0760dc94d50d
ashtell <mhallisawesome@gmail.com>
2015-10-06 20:42:26 -0600

    finished project

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
-    $('.box')on('click', function(){
-        $(this).addClass('white');
-    })
-})
+    var color = 'red blue yellow green white';
+    $('.box').on('click', function(){
+        $(this).addClass(color);
+    });
+    $('.box').on('dblclick', function(){
+        $(this).removeClass(color);
+    });
+    $("#reset").on('click', function(){
+        $(".box").removeClass();
+    });
+    $('#red').on('click', function(){
+        color = 'red';
+    });
+    $('#blue').on('click', function(){
+      color = 'blue';
+    });
+  
+    $('#green').on('click', function(){
+      color = 'green';
+    });
+  
+    $('#yellow').on('click', function(){
+      color = 'yellow';
+    });
+  
+    $('#white').on('click', function(){
+      color = 'white';
+    });
+});
